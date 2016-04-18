420cb360270a344a72a5c1e5cb64e2f4ccf87270
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-04-18 12:52:33 -0700

    Rename to args

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ var os = require('os')
 
 
 function launch(url, options, callback) {
-  var params = []
+  var args = []
 
   // If URL is present, pass it to adaptor that starts Edge with the URL
   if(url && url.length) {
     var urlObj = urlparse(url, true)
     delete urlObj.search   // url.format does not want search attribute
     url = urlformat(urlObj)
-    params.push('-l ' + url)
+    args.push('-l ' + url)
   }
   
   // Fire up process
   var command = path.resolve(__dirname, '..', 'node_modules', 'edge-diagnostics-adapter', 'dist', os.arch(), 'EdgeDiagnosticsAdapter.exe')  
-  var adaptorProcess = child_process.execFile(command, params, callback)
+  var adaptorProcess = child_process.execFile(command, args, callback)
 
   process.on('exit', function(code) {
     adaptorProcess.kill()
