51372be9f4886e6d948be519b7a0531c3c27343d
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-04-18 12:53:32 -0700

    Get rid of comments

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ var os = require('os')
 function launch(url, options, callback) {
   var args = []
 
-  // If URL is present, pass it to adaptor that starts Edge with the URL
   if(url && url.length) {
     var urlObj = urlparse(url, true)
     delete urlObj.search   // url.format does not want search attribute
@@ -16,7 +15,6 @@ function launch(url, options, callback) {
     args.push('-l ' + url)
   }
   
-  // Fire up process
   var command = path.resolve(__dirname, '..', 'node_modules', 'edge-diagnostics-adapter', 'dist', os.arch(), 'EdgeDiagnosticsAdapter.exe')  
   
   return child_process.execFile(command, args, callback)
