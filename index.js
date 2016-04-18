449a01ab6fa86356817a82b5878c7f66e8dcac81
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-04-18 12:54:56 -0700

    StandardJS formatting

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ var child_process = require('child_process')
 var path = require('path')
 var os = require('os')
 
-
-function launch(url, options, callback) {
+function launch (url, options, callback) {
   var args = []
 
-  if(url && url.length) {
+  if (url && url.length) {
     var urlObj = urlparse(url, true)
     delete urlObj.search   // url.format does not want search attribute
     url = urlformat(urlObj)
     args.push('-l ' + url)
   }
-  
-  var command = path.resolve(__dirname, '..', 'node_modules', 'edge-diagnostics-adapter', 'dist', os.arch(), 'EdgeDiagnosticsAdapter.exe')  
-  
+
+  var command = path.resolve(__dirname, '..', 'node_modules', 'edge-diagnostics-adapter', 'dist', os.arch(), 'EdgeDiagnosticsAdapter.exe')
+
   return child_process.execFile(command, args, callback)
 }
 
